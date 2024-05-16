3c8f0cda708bd439d7781d46de9109a90ee91d6c
Laíse Vieira <laisevdev@gmail.com>
2024-05-16 13:58:49 -0300

    [refactor]: def show_characters_houses add 'quotes'

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 import requests
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'api/index.html', {})
 
@@ -10,3 +11,28 @@ def show_characters(request):
     data = response.json()
     characters = [character['name'] for character in data]  
     return render(request, 'api/characters.html', {'characters': characters})
+
+def show_characters_houses(request): 
+    url = 'https://api.gameofthronesquotes.xyz/v1/characters'
+    response = requests.get(url)
+    data = response.json()
+    
+    characters_with_houses = []
+    for character in data:
+        house = character.get('house')
+        if house is not None:
+            house_slug = house.get('slug')
+            if house_slug is not None:
+                quote = character.get('quotes')
+                if quote is not None:
+                    characters_with_houses.append({
+                        'name': character['name'],
+                        'house_slug': house_slug,
+                        'quote': character['quotes'][0]
+                    })    
+    return render(request, 'api/houses_characters.html', {'characters_with_houses': characters_with_houses})
+
+
+
+
+
