0590e5a4fadf5e745ff8a5b9de768855045b129a
Laíse Vieira <laisevdev@gmail.com>
2024-05-15 23:58:11 -0300

    [feat] add index and show_characters views

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 import requests
-from django.http import JsonResponse
+from django.shortcuts import render
 
+def index(request):
+    return render(request, 'api/index.html', {})
 
 def show_characters(request): 
     url = 'https://api.gameofthronesquotes.xyz/v1/characters'
     response = requests.get(url)
     data = response.json()
-    characters = data['name']
-    return JsonResponse(characters)
+    characters = [character['name'] for character in data]  
+    return render(request, 'api/characters.html', {'characters': characters})
