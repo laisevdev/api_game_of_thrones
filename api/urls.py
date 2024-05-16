c188ce067d07df9a4d5aaf48c95a4582fca4f139
Laíse Vieira <laisevdev@gmail.com>
2024-05-16 14:00:15 -0300

    [feat]: add houses_characters url

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/characters/', views.show_characters, name='characters'),
+    path('api/housescharacters/', views.show_characters_houses, name='houses_characters'),
 ]
