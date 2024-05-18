22a80f889f4056c9add4841117c82d0eb3160a94
Laíse Vieira <laisevdev@gmail.com>
2024-05-18 16:22:16 -0300

    [refactor]:change houses_detais to allinformation url path name

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('characters/', views.show_characters, name='characters'),
     path('housescharacters/', views.show_characters_houses, name='houses_characters'),
-    path('housesdetails/', views.houses_details, name='houses_details'),
+    path('allinformations/', views.all_info_about_got, name='all_informations'),
     path('houses/', views.westeros_houses, name='houses'),
     path('details/<slug:slug>/', views.details, name='details'),
 ]
